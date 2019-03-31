21f9b4002759e7c5707345dcf7ac9666b5416815
Yiannis <yiannis@yiannis2.local>
2019-03-31 01:05:54 -0500

    Add documentation on call,apply,bind

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //js
+//main differences here to remember:
+//call    - immediate invoke
+//apply - immediate invoke
+//bind   - return function invoke
 var Sports = {
     "type": "Baseball",
     "team": "Twins",
@@ -14,21 +18,31 @@ var Sports = {
     }
   };
   function switchPhysics(state) {
-    return this.physics + ' are in many smartphones. ' + 'We are currently in a ' + state;
+    return this.physics + ' are in many smartphones. ' 
+      + 'We are currently in a ' + state;
   }
   var switches = switchPhysics.bind(this, 'Black Hole Era');
   
   function PrintTypeWithArgument(b) {
-    return 'Printing this.type with arguments: ' + this.type + ' ' + this.team + ' ' + b * b;
+    return 'Printing this.type with arguments: ' + 
+                this.type + ' ' + this.team + ' ' + b * b;
   }
   var photons = switchPhysics.bind(Sports, 'Black Hole Era');
-  
   var mult = PrintTypeWithArgument.bind(Sports, 3333);
-
   var newSport = new SportAction();
+  
+  switchPhysics.call(Sports,'Rock');
+  
+  PrintTypeWithArgument.apply(Sports,[303,30]);
+  
   //mocha
   mocha.setup('bdd');
   const { assert } = chai;
+  describe('Apply PrintTypeWithArgument', () => {
+    it('Should apply Sports object with array', () => {
+      assert.equal(PrintTypeWithArgument.apply(Sports,[303,30]), "Printing this.type with arguments: Baseball Twins 91809")
+     })
+  })
   describe('Instance should inherit object constructor', () => {
     it('Should be an object instance', () => {
       assert.instanceOf(newSport, SportAction, 'newSport is an instance of SportAction');
@@ -44,6 +58,16 @@ var Sports = {
     })
     it('Object is sealed', () => {
       assert.sealed(sealed);
+    })
+  })
+  describe('Calling switchPhysics', () => {
+    it('calling', () => {
+      assert.equal(switchPhysics.call(Sports,'Rock'), "Photons are in many smartphones. We are currently in a Rock")
+    })
+  })
+  describe('True should assert.equal true', () => {
+    it('True be true', () => {
+      assert.equal(true,true);
     })
   })
   describe('Test instance call Sports', () => {
